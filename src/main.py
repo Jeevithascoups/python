21ce406c72a95775adccbc11d7e1360373e47fb7
root <root@localhost>
2023-12-06 19:29:02 +0530

    auto publish 1701871142370

--- src/main.py
+++ src/main.py
@@ -1,11 +1,10 @@
-len = int(input("How many numbers do you want to enter? "))
+def divisible_by_3and5(num):
+   result = [ ]
+   for i in range(num):
+       if i%3 == 0 and i%5 == 0:
+           result.append(i)
+   return result
  
-nums = []
- 
-for i in range(0, len):
-   element = int(input("Enter element: "))
-   nums.append(element)
- 
-total = sum(nums)
-avg = total/len
-print("Average of elements you entered",round(avg,2))
+num = int (input('Enter your number: '))
+result = divisible_by_3and5(num)
+print(result)
